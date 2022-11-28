0d01121feec26117d5a07d1e41af34344658a970
beyond9thousand <beyond9thousand@gmail.com>
2022-11-28 15:34:04 +0530

    not sure what went wrong

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -35,8 +35,8 @@ module.exports = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          // editUrl:
+          //   "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         blog: {
           showReadingTime: true,
