62f4e8052b5a1b8bcffff20ceed2e18a63538528
beyond9thousand <beyond9thousand@gmail.com>
2022-11-30 18:53:43 +0530

    forgot to re-add files

---
+++ src/pages/index.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
+
+import styles from "../css/index.module.css";
+
+function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx("hero hero--dark", styles.heroBanner)}>
+      <div className="container">
+        <span className="banner_title">{siteConfig.title}</span>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button home_slap button--secondary button--outline button--lg"
+            to="/docs/about"
+          >
+            Introduction
+          </Link>
+          <Link
+            className="button home_slap button--primary button--outline button--lg"
+            to="/docs/start"
+          >
+            Getting started
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout
+      title={`beyond9thousand `}
+      description="Description will go into a meta tag in <head />"
+    >
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
